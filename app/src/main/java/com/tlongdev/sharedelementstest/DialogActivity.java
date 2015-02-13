122abc2f45498a21e49d1a978cec5ffd40006870
Long Tran <lngtrn94@gmail.com>
2015-02-13 22:55:03 +0100

    working transition

--- app/src/main/java/com/tlongdev/sharedelementstest/DialogActivity.java
+++ app/src/main/java/com/tlongdev/sharedelementstest/DialogActivity.java
@@ -2,6 +2,7 @@ package com.tlongdev.sharedelementstest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -18,6 +19,14 @@ public class DialogActivity extends Activity {
         layout.getLayoutParams().width = screenWidth / 3;
         layout.getLayoutParams().height = screenWidth / 3;
         layout.requestLayout();
+
+        ((View)findViewById(R.id.card).getParent()).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAfterTransition();
+            }
+        });
+        findViewById(R.id.card).setOnClickListener(null);
 
         int position = getIntent().getIntExtra("position", 0);
         ((TextView)findViewById(R.id.text)).setText("" + position);
